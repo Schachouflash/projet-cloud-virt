34bfd9e09bc6edab1c5c340b7a93f32852b60456
CIPOLLA NICOLAS <nicolas.cipolla@etu.unistra.fr>
2023-05-19 23:03:19 +0200

    ajout du keepalived dans les scripts

--- scrpitbash/launchcervantes.sh
+++ scrpitbash/launchcervantes.sh
@@ -2,16 +2,22 @@ ssh -A cervantes # se connecter à cervantes
 rm -rf projet-cloud-virt
 git clone git@github.com:schachouflash/projet-cloud-virt.git
 
+sudo apt-get update
+sudo apt install keepalived -y
+
 #supprimer les anciens fichiers de configuration
 sudo rm /etc/nomad.d/nomad.hcl
 sudo rm /etc/consul.d/consul.hcl
+sudo rm /etc/keepalived/keepalived.conf
 
 #copier les nouveaux fichiers de configuration
 sudo cp ~/projet-cloud-virt/confignomadconsul/confconsulcervantes /etc/consul.d/consul.hcl
 sudo cp ~/projet-cloud-virt/confignomadconsul/confnomadcervantes /etc/nomad.d/nomad.hcl
+sudo cp ~/projet-cloud-virt/keepalived.conf /etc/keepalived/keepalived.conf
 
 sudo systemctl restart nomad
 sudo systemctl restart consul
+sudo systemctl restart keepalived
 
 consul join montagne-verte 
 nomad server-join montagne-verte
